2e4d26d708774e0e4515f05ecb3d7065d8622a6f
Abhi06027 <abhinavk67@gmail.com>
2022-02-01 00:36:30 +0530

    netflix responsive

--- src/App.js
+++ src/App.js
@@ -9,14 +9,20 @@ import Appletv from './Components/Appletv';
 import Children from './Components/Children';
 import Asked from './Components/Asked';
 import Footer from './Components/Footer';
-
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
    
 
    <div className=' bg-goli' >
-  
+    <Helmet>
+                <meta charSet="utf-8" />
+                <title>Netflix - Watch TV Shows Online, Watch Movies Online</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+         <meta name="description" content="
+Netflix - Watch TV Shows Online, Watch Movies Online" />
+            </Helmet>
    
     <div className=' bg-black ' >
     <div className='  w-full      md:min-h-[700px] lg:min-h-[700px] bg-bottom bg-cover lg:bg-center  '  style  ={{ backgroundImage: `url(${herobgs})`  }}> 
