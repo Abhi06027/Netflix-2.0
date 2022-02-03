af331affe1988bf2a954ab007788543c6c6b8fc7
Abhi06027 <abhinavk67@gmail.com>
2022-02-04 01:45:47 +0530

    netflix

--- src/Components/Asked.js
+++ src/Components/Asked.js
@@ -1,61 +1,184 @@
-import React from 'react';
+import React from "react";
 
 export default function Asked() {
-  return <div>
-      
-        
-       
+  return (
+    <div>
+      <div className="  mt-2 bg-black text-white   ">
+        <h1 className="text-center text-2xl sm:text-2xl md:text-4xl lg:text-5xl font-bold  pb-3 pt-12  lg:pb-12 lg:pt-12 px-10 ">
+          Frequently Asked Questions
+        </h1>
+        <ul>
+          <li className=" bg-goli  md:max-w-xl lg:max-w-3xl  mx-auto text-lg sm:text-lg md:text-lg lg:text-2xl p-3 sm:p-3 md:p-3 lg:p-6 my-2 flex  items-center justify-between  ">
+            What is Netflix?{" "}
+            <span>
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 4v16m8-8H4"
+                />
+              </svg>{" "}
+            </span>{" "}
+          </li>
 
-      <div className='  mt-2 bg-black text-white   '> 
-      <h1 className='text-center text-2xl sm:text-2xl md:text-4xl lg:text-5xl font-bold  pb-3 pt-12  lg:pb-12 lg:pt-12 px-10 ' >Frequently Asked Questions</h1>
-     <ul> 
-        <li className=' bg-goli  md:max-w-xl lg:max-w-3xl  mx-auto text-lg sm:text-lg md:text-lg lg:text-2xl p-3 sm:p-3 md:p-3 lg:p-6 my-2 flex  items-center justify-between  ' >What is Netflix? <span> <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-</svg>  </span>  </li>
-        
-      <li className=' bg-goli  md:max-w-xl lg:max-w-3xl  mx-auto text-lg sm:text-lg md:text-lg lg:text-2xl p-3 sm:p-3 md:p-3 lg:p-6 my-2 flex  items-center justify-between ' >How much does Netflix cost? <span> <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-        </svg>  </span>   </li>
-    <div  className=' md:max-w-xl lg:max-w-3xl  hidden' >   Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.
+          <li className=" bg-goli  md:max-w-xl lg:max-w-3xl  mx-auto text-lg sm:text-lg md:text-lg lg:text-2xl p-3 sm:p-3 md:p-3 lg:p-6 my-2 flex  items-center justify-between ">
+            How much does Netflix cost?{" "}
+            <span>
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 4v16m8-8H4"
+                />
+              </svg>{" "}
+            </span>{" "}
+          </li>
+          <div className=" md:max-w-xl lg:max-w-3xl  hidden">
+            {" "}
+            Netflix is a streaming service that offers a wide variety of
+            award-winning TV shows, movies, anime, documentaries and more – on
+            thousands of internet-connected devices. You can watch as much as
+            you want, whenever you want, without a single ad – all for one low
+            monthly price. There's always something new to discover, and new TV
+            shows and movies are added every week!{" "}
+          </div>
 
-     You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week! </div>
-   
-
-      <li className=' bg-goli  md:max-w-xl lg:max-w-3xl  mx-auto text-lg sm:text-lg md:text-lg lg:text-2xl p-3 sm:p-3 md:p-3 lg:p-6 my-2 flex  items-center justify-between' >Where can I watch? <span> <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-</svg>  </span> </li>
-      <li className=' bg-goli  md:max-w-xl lg:max-w-3xl  mx-auto text-lg sm:text-lg md:text-lg lg:text-2xl p-3 sm:p-3 md:p-3 lg:p-6 my-2 flex  items-center justify-between ' >How do I cancel? <span> <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-</svg>  </span> </li>
-      <li className=' bg-goli  md:max-w-xl lg:max-w-3xl  mx-auto text-lg sm:text-lg md:text-lg lg:text-2xl p-3 sm:p-3 md:p-3 lg:p-6 my-2 flex  items-center justify-between' >What can I watch on Netflix? <span> <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-</svg>  </span> </li>
-     <li className=' bg-goli  md:max-w-xl lg:max-w-3xl  mx-auto text-lg sm:text-lg md:text-lg lg:text-2xl p-3 sm:p-3 md:p-3 lg:p-6 my-2 flex  items-center justify-between' >Is Netflix good for kids? <span> <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-</svg>  </span> </li>
+          <li className=" bg-goli  md:max-w-xl lg:max-w-3xl  mx-auto text-lg sm:text-lg md:text-lg lg:text-2xl p-3 sm:p-3 md:p-3 lg:p-6 my-2 flex  items-center justify-between">
+            Where can I watch?{" "}
+            <span>
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 4v16m8-8H4"
+                />
+              </svg>{" "}
+            </span>{" "}
+          </li>
+          <li className=" bg-goli  md:max-w-xl lg:max-w-3xl  mx-auto text-lg sm:text-lg md:text-lg lg:text-2xl p-3 sm:p-3 md:p-3 lg:p-6 my-2 flex  items-center justify-between ">
+            How do I cancel?{" "}
+            <span>
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 4v16m8-8H4"
+                />
+              </svg>{" "}
+            </span>{" "}
+          </li>
+          <li className=" bg-goli  md:max-w-xl lg:max-w-3xl  mx-auto text-lg sm:text-lg md:text-lg lg:text-2xl p-3 sm:p-3 md:p-3 lg:p-6 my-2 flex  items-center justify-between">
+            What can I watch on Netflix?{" "}
+            <span>
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 4v16m8-8H4"
+                />
+              </svg>{" "}
+            </span>{" "}
+          </li>
+          <li className=" bg-goli  md:max-w-xl lg:max-w-3xl  mx-auto text-lg sm:text-lg md:text-lg lg:text-2xl p-3 sm:p-3 md:p-3 lg:p-6 my-2 flex  items-center justify-between">
+            Is Netflix good for kids?{" "}
+            <span>
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 4v16m8-8H4"
+                />
+              </svg>{" "}
+            </span>{" "}
+          </li>
         </ul>
-       <main className='flex justify-center items-center flex-col   min-h-[230px] sm:min-h-[250px] md:sm:min-h-[300px]  lg:min-h-[250px] bg-black text-white   '>
-        
-     
-<h1 className='  text-white text-center px-8 sm:px-5 md:px-44 lg:px-5   text-lg sm:text-lg md:text-xl lg:text-xl max-w-3xl  font-bold leading-tight  ' > Ready to watch? Enter your email to create or restart your membership. </h1>
+        <main className="flex justify-center items-center flex-col   min-h-[230px] sm:min-h-[250px] md:sm:min-h-[300px]  lg:min-h-[250px] bg-black text-white   ">
+          <h1 className="  text-white text-center px-8 sm:px-5 md:px-44 lg:px-5   text-lg sm:text-lg md:text-xl lg:text-xl max-w-3xl  font-bold leading-tight  ">
+            {" "}
+            Ready to watch? Enter your email to create or restart your
+            membership.{" "}
+          </h1>
 
+          <div className=" flex flex-col justify-center items-center sm:flex sm:flex-col sm:justify-center sm:items-center md:flex md:flex-col md:justify-center md:items-center lg:flex lg:flex-row lg:justify-center lg:items-center ">
+            <input
+              className="min-w-[365px] sm:min-w-[500px]  md:min-w-[500px]   lg:min-w-[500px] mt-4 h-12  lg:h-16  pl-3 outline-none focus:ring-1 focus:ring-blue-500 z-10  "
+              type="email"
+              placeholder="Email address"
+            />
 
-
-<div className=' flex flex-col justify-center items-center sm:flex sm:flex-col sm:justify-center sm:items-center md:flex md:flex-col md:justify-center md:items-center lg:flex lg:flex-row lg:justify-center lg:items-center '  > 
-<input className='min-w-[365px] sm:min-w-[500px]  md:min-w-[500px]   lg:min-w-[500px] mt-4 h-12  lg:h-16  pl-3 outline-none focus:ring-1 focus:ring-blue-500 z-10  ' type="email" placeholder="Email address"/>
-
-<div className='  mt-4 ' >
-<a href="https://www.netflix.com/in/">
-<button className=' flex justify-center items-center bg-netflixred  hover:bg-red-600  h-10 w-32 text-base  lg:h-16 lg:w-56 lg:text-3xl text-white ' > Get Started     
-<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4   lg:h-7 lg:w-7 pt-1 pl-1 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-</svg>
-</button>
-</a>
-</div>
-</div>
-
-</main>
- </div>
-  </div>;
+            <div className="  mt-4 ">
+              <a href="https://abhi06027.github.io/Avatar/">
+                <button className=" flex justify-center items-center bg-netflixred  hover:bg-red-600  h-10 w-32 text-base  lg:h-16 lg:w-56 lg:text-3xl text-white ">
+                  {" "}
+                  Get Started
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-4 w-4   lg:h-7 lg:w-7 pt-1 pl-1 "
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </button>
+              </a>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,27 +1,52 @@
-import React from 'react';
+import React from "react";
 
 export default function Main() {
-  return <div>
+  return (
+    <div>
+      <main className="flex justify-center  py-16  lg:py-32 items-center flex-col flex-1 text-center px-3 sm:px-5 md:px-10 lg:px-10   ">
+        <h1 className=" text-3xl sm:text-6xl md:text-6xl  lg:text-6xl font-semibold text-white   lg:max-w-2xl  brightness-200 leading-tight  ">
+          {" "}
+          Unlimited movies, TV shows and more.
+        </h1>
+        <h2 className=" text-lg sm:text-lg md:text-2xl  lg:text-2xl mt-5  text-white brightness-200 leading-tight ">
+          {" "}
+          Watch anywhere. Cancel anytime..
+        </h2>
+        <p className=" text-lg sm:text-lg md:text-2xl lg:text-xl mt-3 px-8 sm:px-8 md:px-36 lg:px-10   text-white  brightness-200 leading-tight ">
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
 
-<main className='flex justify-center  py-16  lg:py-32 items-center flex-col flex-1 text-center px-3 sm:px-5 md:px-10 lg:px-10   '  >
-<h1 className=' text-3xl sm:text-6xl md:text-6xl  lg:text-6xl font-semibold text-white   lg:max-w-2xl  brightness-200 leading-tight  '  > Unlimited movies, TV shows and more.</h1>
-<h2 className=' text-lg sm:text-lg md:text-2xl  lg:text-2xl mt-5  text-white brightness-200 leading-tight '  > Watch anywhere. Cancel anytime..</h2>
-<p className=' text-lg sm:text-lg md:text-2xl lg:text-xl mt-3 px-8 sm:px-8 md:px-36 lg:px-10   text-white  brightness-200 leading-tight '  >Ready to watch? Enter your email to create or restart your membership.</p>
+        <div className=" flex  flex-col justify-center items-center sm:flex sm:flex-col sm:justify-center sm:items-center md:flex md:flex-col md:justify-center md:items-center lg:flex lg:flex-row lg:justify-center lg:items-center  ">
+          <input
+            className=" min-w-[355px] sm:min-w-[400px] md:min-w-[500px] lg:min-w-[550px ] max-w-3xl mx-auto mt-4 h-12 lg:h-16 pl-3 outline-none focus:ring-1 focus:ring-blue-500 z-10  "
+            type="email"
+            placeholder="Email address"
+          />
 
-<div className=' flex  flex-col justify-center items-center sm:flex sm:flex-col sm:justify-center sm:items-center md:flex md:flex-col md:justify-center md:items-center lg:flex lg:flex-row lg:justify-center lg:items-center  '  > 
-<input className=' min-w-[355px] sm:min-w-[400px] md:min-w-[500px] lg:min-w-[550px ] max-w-3xl mx-auto mt-4 h-12 lg:h-16 pl-3 outline-none focus:ring-1 focus:ring-blue-500 z-10  ' type="email" placeholder="Email address"/>
-
-<div className='mt-4'>
-
-<a href="https://www.netflix.com/in/">
-<button className=' flex justify-center items-center bg-netflixred  hover:bg-red-600  h-10 w-32 text-base   lg:h-16 lg:w-56 lg:text-3xl text-white ' > Get Started     
-<svg xmlns="http://www.w3.org/2000/svg" class=" flex items-center  h-4 w-4   lg:h-7 lg:w-7 pt-1 pl-1 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-</svg>
-</button>
-</a>
-</div>
-</div>
-</main>
-</div>;
+          <div className="mt-4">
+            <a href="https://abhi06027.github.io/Avatar/">
+              <button className=" flex justify-center items-center bg-netflixred  hover:bg-red-600  h-10 w-32 text-base   lg:h-16 lg:w-56 lg:text-3xl text-white ">
+                {" "}
+                Get Started
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class=" flex items-center  h-4 w-4   lg:h-7 lg:w-7 pt-1 pl-1 "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 5l7 7-7 7"
+                  />
+                </svg>
+              </button>
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
